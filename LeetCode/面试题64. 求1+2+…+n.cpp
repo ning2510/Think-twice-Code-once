b99367dc643af1df0f045bdcbe76f1603a592bfc
ning2510 <1367560112@qq.com>
2020-06-02 11:08:19 +0800

    《剑指Offer》

---
+++ LeetCode/面试题64. 求1+2+…+n.cpp
@@ -0,0 +1,29 @@
+// 方法一：逻辑运算符 + 递归
+class Solution {
+public:
+    int ans = 0;
+
+    int sumNums(int n) {
+        bool x = n > 1 && sumNums(n - 1) > 0;   //若 n <= 1，则不会进行 sumNums(0)
+        ans += n;
+        return ans;
+    }
+};
+
+/* 方法二：快速乘
+class Solution {
+public:
+    int ksc(int a, int b) {
+        int ans = 1, base = a;
+        while(b > 0) {
+            if(b & 1) ans += base;
+            base += base;
+            b >>= 1;
+        }
+        return ans;
+    }
+
+    int sumNums(int n) {
+        return (ksc(1 + n, n) >> 1);
+    }
+};*/
