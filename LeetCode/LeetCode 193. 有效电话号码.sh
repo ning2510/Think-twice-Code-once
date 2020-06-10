751935ea86dbccd75d9dd0a908df923b20c90606
ning2510 <1367560112@qq.com>
2020-06-10 18:20:02 +0800

    Shell

--- LeetCode/LeetCode 193. 有效电话号码.sh
+++ LeetCode/LeetCode 193. 有效电话号码.sh
@@ -1,3 +1,7 @@
 # Read from the file file.txt and output all valid phone numbers to stdout.
 
-cat file.txt | grep -P '^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$' file.txt
+cat file.txt | grep -P '([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}' file.txt
+# -P：利用PERL的正则语法进行匹配
+# {3}：前面的数重复3次
+# \(\)：取消括号的特殊含有
+# |：指明两项之间的一个选择
