f9059950309fe2e210b91ae2ea1faa9a4124bd2c
ning2510 <1367560112@qq.com>
2020-06-16 15:01:26 +0800

    《程序员面试金典》

---
+++ LeetCode/面试题10.09. 排序矩阵查找.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        if(!n) return false;
+        int m = matrix[0].size();
+        int x = 0, y = m - 1;
+        while(x < n && y >= 0) {
+            if(matrix[x][y] == target) return true;
+            else if(matrix[x][y] < target) x++;
+            else y--;
+        }
+        return false;
+    }
+};
