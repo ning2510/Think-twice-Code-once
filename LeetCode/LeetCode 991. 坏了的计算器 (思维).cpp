0ed2ac6766a7432344ba365a46fcb4a62b902133
ning2510 <1367560112@qq.com>
2020-08-01 16:35:52 +0800

    思维

---
+++ LeetCode/LeetCode 991. 坏了的计算器 (思维).cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int ans = 0;
+        while(Y > X) {
+            ans++;
+            if(Y & 1) Y++;
+            else Y >>= 1;
+        }
+        return ans + X - Y;
+    }
+};
