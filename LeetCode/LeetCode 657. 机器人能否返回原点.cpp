c5739044eb6f8ad2b64747250356280b30d4e7ac
ning2510 <1367560112@qq.com>
2020-08-28 10:23:42 +0800

    每日一题

---
+++ LeetCode/LeetCode 657. 机器人能否返回原点.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for(auto &c : moves) {
+            if(c == 'U') y++;
+            else if(c == 'D') y--;
+            else if(c == 'L') x--;
+            else x++;
+        }
+        if(x == 0 && y == 0) return true;
+        return false;
+    }
+};
