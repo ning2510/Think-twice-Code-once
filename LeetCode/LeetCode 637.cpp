5baf4e22811b7a15c90f5c4f7f022d360ce68d5e
ning2510 <1367560112@qq.com>
2020-09-12 09:09:17 +0800

    树

--- LeetCode/LeetCode 637.cpp
+++ LeetCode/LeetCode 637.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// 方法一: DFS
 class Solution {
     unordered_map <long long, long long> ss, sum;
     int max_deep = 0;
@@ -27,6 +28,40 @@ public:
         for(int i = 0; i <= max_deep; i++) {
             double x = (double)sum[i] / (double)ss[i];
             ans.push_back(x);
+        }
+        return ans;
+    }
+};
+
+// 方法二: BFS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector <double> ans;
+        queue <TreeNode*> ss;
+        if(!root) return ans;
+        ss.push(root);
+        while(!ss.empty()) {
+            int n = ss.size();
+            double cnt = 0, sum = 0;
+            for(int i = 1; i <= n; i++) {
+                auto k = ss.front();
+                ss.pop();
+                cnt++;
+                sum += k->val;
+                if(k->left) ss.push(k->left);
+                if(k->right) ss.push(k->right);
+            }
+            ans.push_back(sum / cnt);
         }
         return ans;
     }
